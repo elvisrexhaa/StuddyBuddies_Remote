19559af301ce86e2d4c83ecabff6c328cecaf0ff
ELvis Reja <elvisrexha2000@gmail.com>
2022-11-02 13:10:12 +0000

    Initial Commit

---
+++ StuddyBuddies/StuddyBuddiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StuddyBuddiesApp.swift
+//  StuddyBuddies
+//
+//  Created by Elvis Rexha on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct StuddyBuddiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
